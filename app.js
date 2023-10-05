e8c4f6872ec96e47a9255b4549dc01c706dff55d
Attasiem Kingsley <attasiemj@gmail.com>
2023-10-05 01:05:55 +0100

    fix: response msgs

--- app.js
+++ app.js
@@ -55,49 +55,100 @@ app.get("/webhook", (req, res, next) => {
   }
 });
 
-app.post("/webhook", (req, res, next) => {
+app.post("/webhook", (req, res) => {
+  //i want some
+
   let body_param = req.body;
+
   console.log(JSON.stringify(body_param, null, 2));
 
   if (body_param.object) {
-    console.log("gets here one");
+    console.log("inside body param");
     if (
       body_param.entry &&
+      body_param.entry[0].changes &&
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-        let phone_numhber_id =
+      let phon_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = body_param.entry[0].changes[0].value.messages[0].from;
-        let msg_body =
-        body_param.entry[0].changes[0].value.messages[0].text.body;
-        console.log("gets here two",phone_numhber_id,from,msg_body);
+      let from = body_param.entry[0].changes[0].value.messages[0].from;
+      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+      console.log("phone number " + phon_no_id);
+      console.log("from " + from);
+      console.log("boady param " + msg_body);
 
       axios({
         method: "POST",
         url:
-          "https://graph.facebook.com/v17.0/" +
-          phone_numhber_id +
+          "https://graph.facebook.com/v13.0/" +
+          phon_no_id +
           "/messages?access_token=" +
           token,
         data: {
           messaging_product: "whatsapp",
           to: from,
           text: {
-            body: "Hi.. I am James",
+            body: "Hi.. I'm James, your message is " + msg_body,
           },
         },
         headers: {
           "Content-Type": "application/json",
         },
       });
-      console.log("gets here three");
+
       res.sendStatus(200);
     } else {
       res.sendStatus(404);
     }
   }
 });
+
+// app.post("/webhook", (req, res, next) => {
+//   let body_param = req.body;
+//   console.log(JSON.stringify(body_param, null, 2));
+
+//   if (body_param.object) {
+//     console.log("gets here one");
+//     if (
+//       body_param.entry &&
+//       body_param.entry[0].changes[0].value.messages &&
+//       body_param.entry[0].changes[0].value.messages[0]
+//     ) {
+//         let phone_numhber_id =
+//         body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//         let from = body_param.entry[0].changes[0].value.messages[0].from;
+//         let msg_body =
+//         body_param.entry[0].changes[0].value.messages[0].text.body;
+//         console.log("gets here two",phone_numhber_id,from,msg_body);
+
+//       axios({
+//         method: "POST",
+//         url:
+//           "https://graph.facebook.com/v17.0/" +
+//           phone_numhber_id +
+//           "/messages?access_token=" +
+//           token,
+//         data: {
+//           messaging_product: "whatsapp",
+//           to: from,
+//           text: {
+//             body: "Hi.. I am James",
+//           },
+//         },
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       console.log("gets here three");
+//       res.sendStatus(200);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   }
+// });
 
 //handling errors
 app.use((req, res, next) => {
